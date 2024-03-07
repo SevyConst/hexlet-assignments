f40d99160c0852586fb7c0554ec2a12a3a376d8a
@hexlet/cli <support@hexlet.io>
2024-03-07 19:43:27 +0300

    submit java-spring-ru/dto-mapper

---
+++ java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -0,0 +1,34 @@
+package exercise.mapper;
+
+// BEGIN
+
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.model.Product;
+import org.mapstruct.*;
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+abstract public class ProductMapper {
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "barcode", source = "vendorCode")
+    public abstract Product map(ProductCreateDTO dto);
+
+    // Наследует конфигурацию оригинального метода map
+    // Нам не нужно копировать @Mapping(target = "title", source = "name")
+    @Mapping(target = "cost", source = "price")
+    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product model);
+
+    // Здесь можно применить обратное наследование
+    // @InheritInverseConfiguration
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "price", source = "cost")
+    @Mapping(target = "vendorCode", source = "barcode")
+    public abstract ProductDTO map(Product model);
+}
+// END
